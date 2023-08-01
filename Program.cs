7d553f21312f5bbbae59fb605f3db9d4b90ae549
gor <student.ggs@mail.ru>
2023-08-01 17:10:38 +0400

    homework sem5

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 1*/
+
+/*int [] numbers = {345, 568, 314, 789,980,456};
+int result = 0;
+foreach ( int number in numbers)
+{
+    if ((number % 2) == 0)
+    {
+        result ++ ;
+    }
+}
+Console.WriteLine($"Число четных элементов: {result} ");*/
+
+/*Задача 2 */
+
+/*int [] Array = {8,7,0,1,-4,-5};
+int Sum = 0;
+for (int i = 0 ; i < Array.Length;i ++)
+{
+    if ((i % 2) != 0)
+    {
+        
+        Sum += Array[i];
+    }
+}
+Console.WriteLine($"{Sum}");*/
+
+/*Задача 3*/
+
+/*int [] Array = {5.56; 7.18;-3.5; 8.5;10.15 };
+int minValue = Array[0];
+int maxValue = Array[0];
+int raz ;
+for (int i = 0; i< Array.Length; i ++)
+{
+    if (Array[i] < minValue)
+    {
+        minValue = Array[i]; 
+    } 
+    else if (Array[i]> maxValue)
+    {
+        maxValue = Array[i];
+    }
+    raz = maxValue-minValue;
+}
+Console.WriteLine($"{raz}");*/
+    
